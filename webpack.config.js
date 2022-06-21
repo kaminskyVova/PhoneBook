881ff769ab568d502d2f032defbeee9c6caddd0f
Kaminsky <kaminskyfrontend@gmail.com>
2022-06-21 21:26:22 +0300

    webP finish

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          // 'postcss-loader',
+          'postcss-loader',
           'sass-loader',
         ],
       },
